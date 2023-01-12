f4ce28e9f7e3a84738ae5b52ffc5a855220a24c4
Mohammad Altamash <ultamesh@gmail.com>
2023-01-13 01:01:19 +0500

    Update

--- src/main/java/com/screenscraper/service/ScreenScraperServiceImpl.java
+++ src/main/java/com/screenscraper/service/ScreenScraperServiceImpl.java
@@ -37,16 +37,16 @@ public class ScreenScraperServiceImpl implements ScreenScraperService {
 
     @Override
     public List<RecordDTO> getData() {
-//        String baseUrl = "file:///C:/workspace/Client/Mr.%20Shahab/Screen%20scraper/virginia-sales.html";
-        String baseUrl = "/virginia-sales.html";
+        String baseUrl = "file:///C:/workspace/Client/Mr.%20Shahab/Screen%20scraper/virginia-sales.html";
+//        String baseUrl = "/virginia-sales.html";
 //        String baseUrl = "http://www.bww-law.com/virginia-sales/";
         WebClient webClient = Utility.getWebClient(USE_INSECURE_SSL, SET_CSS_ENABLED, JAVA_SCRIPT_ENABLED);
         List<RecordDTO> list = new ArrayList<>();
         try {
 //            String searchUrl = baseUrl + "search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
 //            webClient.waitForBackgroundJavaScript(20000);
-//            HtmlPage htmlPage = webClient.getPage(baseUrl);
-            HtmlPage htmlPage = webClient.getPage(ScreenScraperServiceImpl.class.getResource(baseUrl));
+            HtmlPage htmlPage = webClient.getPage(baseUrl);
+//            HtmlPage htmlPage = webClient.getPage(ScreenScraperServiceImpl.class.getResource(baseUrl));
 
 
             List<HtmlElement> items = htmlPage.getByXPath("//td[@class='bodytext']");
